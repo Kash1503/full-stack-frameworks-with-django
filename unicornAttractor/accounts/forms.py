96baa9b63da0ce8171e92bca6d770523c24e89e3
Kashif Javed <Kash@Kashifs-MacBook-Pro.local>
2019-07-10 20:22:50 +0100

    finalise form styling and apply it to register.html and login.html for consistency

--- unicornAttractor/accounts/forms.py
+++ unicornAttractor/accounts/forms.py
@@ -1,7 +1,28 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
     """Form to allow users to input log in details and log in"""
 
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistrationForm(UserCreationForm):
+    """Form to allow users to input their details and create a new account"""
+
+    password1 = forms.CharField(label='Password *', widget=forms.PasswordInput, label_suffix='')
+    password2 = forms.CharField(label='Confirm your password *', widget=forms.PasswordInput, label_suffix='')
+    firstName = forms.CharField(label='First Name *', label_suffix='')
+    lastName = forms.CharField(label='Last Name *', label_suffix='')
+    address1 = forms.CharField(label='Address 1', required=False, label_suffix='')
+    address2 = forms.CharField(label='Address 2', required=False, label_suffix='')
+    city = forms.CharField(required=False, label_suffix='')
+    postcode = forms.CharField(required=False, label_suffix='')
+    username = forms.CharField(label='Username *', label_suffix='')
+    email = forms.CharField(label='Email Address *', label_suffix='')
+
+    class Meta:
+        model = User
+        fields = ['username', 'firstName', 'lastName', 'email', 'password1', 'password2', 'address1', 'address2', 'city', 'postcode']
--- unicornAttractor/accounts/views.py
+++ unicornAttractor/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -17,6 +17,9 @@ def logout(request):
 
 def login(request):
     """Function to allow users to log in"""
+
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
 
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
@@ -36,3 +39,10 @@ def login(request):
         login_form = LoginForm()
 
     return render(request, 'login.html', {'login_form': login_form})
+
+
+def register(request):
+    """Render the registration form and handle registration logic"""
+
+    registration_form = RegistrationForm()
+    return render(request, 'register.html', {'registration_form': registration_form})
