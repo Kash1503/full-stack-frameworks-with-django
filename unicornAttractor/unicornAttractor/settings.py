0ac32058895181b0d21d6351d5318ae940955e7d
Kashif Javed <Kash@Kashifs-MacBook-Pro.local>
2019-07-11 08:27:55 +0100

    Set up real email to be sent when requesting a password reset

--- unicornAttractor/unicornAttractor/settings.py
+++ unicornAttractor/unicornAttractor/settings.py
@@ -126,4 +126,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_RESET_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_RESET_PASSWORD')
+EMAIL_PORT = 587
