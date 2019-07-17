baee4bc74325da02e39e26777a48766bdf47699d
Kashif Javed <Kash@Kashifs-MacBook-Pro.local>
2019-07-17 08:52:49 +0100

    Add checkout app and implement Stripe to make payments.

--- unicornAttractor/checkout/forms.py
+++ unicornAttractor/checkout/forms.py
@@ -6,14 +6,14 @@ class MakePaymentForm(forms.Form):
     Allow users to input payment information to complete the order
     """
 
-    MONTH_CHOICES = [(i, i) for i in range (1, 12)]
+    MONTH_CHOICES = [(i, i) for i in range (1, 13)]
     YEAR_CHOICES = [(i, i) for i in range (2019, 2038)]
 
-    card_number = forms.CharField(label='Card number', required=False)
+    credit_card_number = forms.CharField(label='Card number', required=False)
     cvv = forms.CharField(label='Security code (CVV)', required=False)
-    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
-    stripe_id = forms.CharField(widget=forms.HiddenInput)
+    expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False, widget=forms.Select(attrs={'class': 'form-dropdown'}))
+    expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False, widget=forms.Select(attrs={'class': 'form-dropdown'}))
+    stripe_id = forms.CharField(widget=forms.HiddenInput, required=False)
 
 
 class OrderForm(forms.ModelForm):
@@ -22,4 +22,9 @@ class OrderForm(forms.ModelForm):
     """
     class Meta:
         model = Order
-        fields = {'full_name', 'phone_number', 'country', 'county', 'town_or_city', 'postcode', 'street1', 'street2'}
+        fields = {'full_name', 'phone_number', 'country', 'county', 'town_or_city', 'postcode', 'street1', 'street2', 'country'}
+        labels = {
+            'street1': 'Address',
+            'street2': 'Address 2',
+            'town_or_city': 'Town/City',
+        }
--- unicornAttractor/checkout/views.py
+++ unicornAttractor/checkout/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 import stripe
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.conf import settings
+from django.utils import timezone
 from .forms import OrderForm, MakePaymentForm
+from tickets.models import Ticket
+from .models import OrderLineItem
 
 # Create your views here.
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -14,10 +18,56 @@ def checkout(request):
     """
 
     if request.method == 'POST':
-        order_form = OrderForm(request.POST)
-        payment_form = MakePaymentForm(request.POST)
-    else:
-        order_form = OrderForm()
-        payment_form = MakePaymentForm()
+        order_form = OrderForm(request.POST, label_suffix='')
+        payment_form = MakePaymentForm(request.POST, label_suffix='')
 
-    return render(request, 'checkout.html', {'order_form': order_form, 'payment_form': payment_form})
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date_ordered = timezone.now()
+            order.save()
+
+            cart = request.session.get('cart', {})
+            total_pledge = 0
+            for id, pledge in cart.items():
+                ticket = get_object_or_404(Ticket, pk=id)
+                total_pledge += pledge
+                order_line_item = OrderLineItem(
+                    order=order,
+                    ticket=ticket,
+                    pledge=pledge
+                )
+                order_line_item.save()
+
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total_pledge * 100),
+                    currency = 'GBP',
+                    description=request.user.email,
+                    source=payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, 'Your card was declined!')
+
+            if customer.paid:
+                messages.error(request, 'You have successfully paid!')
+
+                for id, pledge in cart.items():
+                    ticket = get_object_or_404(Ticket, pk=id)
+                    ticket.value += pledge
+                    ticket.save()
+
+                request.session['cart'] = {}
+                return redirect(reverse('tracker'))
+            else:
+                messages.error(request, 'Unable to take payment!')
+        
+        else:
+            print(payment_form.errors)
+            print(order_form.errors)
+            messages.error(request, 'Unable to take payment with that card!')
+
+    else:
+        order_form = OrderForm(label_suffix='')
+        payment_form = MakePaymentForm(label_suffix='')
+
+    return render(request, 'checkout.html', {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE_KEY})
