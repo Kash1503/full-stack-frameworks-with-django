879baeae89fc73b96ca5e0db62e5c2752a03d216
Kashif Javed <Kash@Kashifs-MacBook-Pro.local>
2019-07-18 21:22:15 +0100

    Add dc.js, d3.js, queue.js and crossfilter.js to create dashboard. Basic charts.js file added

--- unicornAttractor/analytics/views.py
+++ unicornAttractor/analytics/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from tickets.models import Ticket
-from django.core import serializers
+from django.core.serializers import serialize
 from django.http import HttpResponse
 
 # Create your views here.
 def get_json_data(request):
     """
-    Query the database and return the data in JSON format to be used by DC/D3.js
+    Query the database and return all closed tickets in JSON format to be used by D3.js
     """
 
-    tickets = Ticket.objects.all()
-    tickets = serializers.serialize('json', tickets)
-    return HttpResponse(tickets, content_type='application/json')
+    closed_tickets = serialize('json', Ticket.objects.filter(status__exact='closed'))
+    return HttpResponse(closed_tickets, content_type="application/json")
 
 
 def dashboard(request):
@@ -19,6 +18,9 @@ def dashboard(request):
     Render the data dashboard and pass through any relevant data
     """
 
-    return render(request, 'dashboard.html')
+    top_features = Ticket.objects.filter(ticket_type__exact='feature').exclude(status__exact='closed').order_by('-value')[:5]
+    top_bugs = Ticket.objects.filter(ticket_type__exact='bug').exclude(status__exact='closed').order_by('-upvotes')[:5]
+
+    return render(request, 'dashboard.html', {'top_features': top_features, 'top_bugs': top_bugs})
 
 
---
+++ unicornAttractor/static/js/charts.js
@@ -0,0 +1,29 @@
+queue()
+    .defer(d3.json, "get_data")
+    .await(makeGraphs);
+
+function makeGraphs(error, graphData){
+
+    var ndx = crossfilter(graphData);
+
+    statusChart(ndx);
+    
+    dc.renderAll();
+}
+
+function statusChart(ndx){
+    
+    var dim = ndx.dimension(dc.pluck('status'));
+    var group = dim.group();
+    
+    dc.pieChart('#status-chart')
+        .width(500)
+        .height(350)
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .legend(dc.legend().x(400).y(50).itemHeight(10).gap(5))
+        .externalRadiusPadding(50)
+        .minAngleForLabel(361);
+}
+    
