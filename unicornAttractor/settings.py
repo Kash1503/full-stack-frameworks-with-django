891b3536dd4e916b7ad7d658ce63e50001f0ba91
Kashif Javed <Kash@Kashifs-MacBook-Pro.local>
2019-07-25 11:59:05 +0100

    Amend STATIC_ROOT and python version in .travis.yml

--- unicornAttractor/settings.py
+++ unicornAttractor/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(STATICFILES_DIRS, 'static')
 
 # Django message storage
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
