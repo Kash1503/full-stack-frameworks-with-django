d90ca45a0cdbb5bf0055a15d93a81f8b080c99f9
Kashif Javed <Kash@Kashifs-MacBook-Pro.local>
2019-07-25 13:51:44 +0100

    Rearrange order of static file settings in settings.py to be in line with the Heroku documentation

--- unicornAttractor/settings.py
+++ unicornAttractor/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Django message storage
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
